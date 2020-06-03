6b152758b65009e7260474ac5efb8368267ff176
Francisco <j.f.maurino@gmail.com>
2020-06-03 11:28:14 -0300

    Termino metodo CrearStateRowLlegadaAvion, cierro loop for de simulate.

--- SistemasDinamicos/SistemasDinamicos/Simulator.cs
+++ SistemasDinamicos/SistemasDinamicos/Simulator.cs
@@ -31,50 +31,88 @@ namespace SimulacionMontecarlo
         }
 
 
-        public IList<StateRow> simulate(int quantity, int from, StateRow initialize)
+        public IList<StateRow> simulate(int quantity, int from, StateRow anterior)
         {
             IList<StateRow> stateRows = new List<StateRow>();
-
-            Dictionary<string, double> tiempos = new Dictionary<string, double>();
-            tiempos.Add("tiempoLlegada", initialize.tiempoProximaLlegada);
-            for (int i = 0; i<initialize.clientes.Count; i++)
-            {
-                tiempos.Add("tiempoPermanencia"+(i+1).ToString(), initialize.clientes[i].tiempoPermanencia);
-            }
-
-            KeyValuePair<string, double> tiempoProximoEvento = tiempos.FirstOrDefault(x => x.Value == tiempos.Values.Min());
-
-            switch (tiempoProximoEvento.Key)
-            {
-                case "tiempoLlegada":
-                    StateRow actual = CrearStateRowLlegadaAvion(initialize, tiempoProximoEvento.Value);
-                    break;
-                default:
-                    break;
-            }
+            StateRow actual = new StateRow();
 
             for (int i=0; i<quantity; i++)
             {
-               
+                // Se arma diccionario con todos los tiempos del vector para determinar el menor, es decir, el siguiente evento
+                Dictionary<string, double> tiempos = new Dictionary<string, double>();
+                if (anterior.tiempoProximaLlegada != 0)
+                    tiempos.Add("tiempoProximaLlegada", anterior.tiempoProximaLlegada);
+                if (anterior.tiempoFinAterrizaje != 0)
+                    tiempos.Add("tiempoFinAterrizaje", anterior.tiempoFinAterrizaje);
+                if (anterior.tiempoFinDeDespegue != 0)
+                    tiempos.Add("tiempoFinDeDespegue", anterior.tiempoFinDeDespegue);
+                for (int j = 0; j < anterior.clientes.Count; j++)
+                {
+                    if (anterior.clientes[j].tiempoPermanencia != 0)
+                        tiempos.Add("tiempoPermanencia_" + (j + 1).ToString(), anterior.clientes[j].tiempoPermanencia);
+                }
+
+                // Obtiene diccionario de tiempos ordenado segun menor tiempo
+                var tiemposOrdenados = tiempos.OrderBy(obj => obj.Value).ToDictionary(obj => obj.Key, obj => obj.Value);
+                KeyValuePair<string, double> menorTiempo = tiemposOrdenados.First();
+                
+                // Controlamos que los aviones en tierra sean menores a 30, si lo son, pasamos al siguiente menor tiempo, es decir, el siguiente evento
+                while (menorTiempo.Key == "tiempoProximaLlegada" && (anterior.pista.colaEET.Count + GetCantidadAvionesEnPermanencia(anterior) >= 30))
+                {
+                    tiemposOrdenados.Remove(tiemposOrdenados.First().Key);
+                    menorTiempo = tiemposOrdenados.First();
+                }
+
+                // Se crea nuevo staterow segun el evento siguiente determinado
+                switch (menorTiempo.Key)
+                {
+                    case "tiempoProximaLlegada":
+                        actual = CrearStateRowLlegadaAvion(anterior, menorTiempo.Value);
+                        actual.iterationNum = i + 1;
+                        break;
+                    case "tiempoFinAterrizaje":
+                        actual = CrearStateRowFinAterrizaje(anterior, menorTiempo.Value);
+                        break;
+                    case "tiempoFinDeDespegue":
+                        actual = CrearStateRowFinDeDespegue(anterior, menorTiempo.Value);
+                        break;
+                    default:
+                        int avion = Convert.ToInt32(menorTiempo.Key.Split('_')[1]);
+                        actual = CrearStateRowFinDePermanencia(anterior, menorTiempo.Value, avion);
+                        break;
+                }
+
+                #region Agregar al showed
+                /*
                 if ((i >= from-1 && i <= from + 99) || i == (quantity - 1))
                 {
                     StateRow row = new StateRow { };
 
                     stateRows.Add(row);
                 }
+                */
+                #endregion
+
+                anterior = actual;
             }
 
             return stateRows;
         }
 
+        private int GetCantidadAvionesEnPermanencia(StateRow vector)
+        {
+            int contador = 0;
+            for (int i=0; i<vector.clientes.Count; i++)
+            {
+                if (vector.clientes[i].estado == "EP")
+                    contador += 1;
+            }
+            return contador;
+        }
+
         private StateRow CrearStateRowLlegadaAvion(StateRow anterior, double tiempoProximoEvento)
         {
             Avion avionNuevo = new Avion();
-            /*
-             *              Solo tener en cuenta cuando la suma de los aviones esperando en tierra y en las colas (o como sea que diga
-             *              la consigna) sea menor a 30.
-             */
-
             StateRow nuevo = new StateRow();
 
             nuevo.evento = "Llegada Avion (" + Avion.count.ToString() + ")";
@@ -115,6 +153,21 @@ namespace SimulacionMontecarlo
             
 
             return nuevo;
+        }
+
+        private StateRow CrearStateRowFinDeDespegue(StateRow anterior, double value)
+        {
+            return new StateRow();
+        }
+
+        private StateRow CrearStateRowFinAterrizaje(StateRow anterior, double value)
+        {
+            return new StateRow();
+        }
+
+        private StateRow CrearStateRowFinDePermanencia(StateRow anterior, double value, int avion)
+        {
+            return new StateRow();
         }
     }
 }
